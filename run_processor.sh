30dd2549bd0a4e20bac7fd33ca0102c8c6e0407a
xim0608 <xim.ryuki@gmail.com>
2018-06-09 19:26:47 +0900

    retrying..

--- run_processor.sh
+++ run_processor.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-export PATH="/usr/local/rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-rbenv exec bundle exec rails runner 'Kuroko2::Servers::WorkflowProcessor.new(log: nil, daemonize: false).run'
+#export PATH="/usr/local/rbenv/bin:$PATH"
+#echo $(which rbenv)
+eval "$(/usr/local/rbenv/bin/rbenv init -)"
+/usr/local/rbenv/bin/rbenv exec bundle exec rails runner 'Kuroko2::Servers::WorkflowProcessor.new(log: nil, daemonize: false).run'
