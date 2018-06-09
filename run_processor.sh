39916608304747ae4900f29c8e24f81274b48195
xim0608 <xim.ryuki@gmail.com>
2018-06-09 19:35:13 +0900

    add setting

--- run_processor.sh
+++ run_processor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH="/usr/local/rbenv/bin:$PATH"
-echo $(which rbenv)
+export RBENV_ROOT=/usr/local/rbenv
+export PATH="$RBENV_ROOT/bin:$PATH"
 eval "$(rbenv init -)"
 rbenv exec bundle exec rails runner 'Kuroko2::Servers::WorkflowProcessor.new(log: nil, daemonize: false).run'
